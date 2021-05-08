df0c743fb9bcf1a479ec7c28310a1a9d747c238e
Lunerio <lunerio@outlook.es>
2021-05-08 16:56:48 -0300

    Endpoint /stats for retrieving count of classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,26 @@
 """Return a JSON status:ok"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    classes = {"amenities": Amenity, "cities": City,\
+               "places": Place, "reviews": Review,\
+               "states": State, "users": User}
+    for i in classes:
+        classes[i] = storage.count(classes[i])
+
+    return jsonify(classes)
