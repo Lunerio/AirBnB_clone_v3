6c6bf3886e58b1d0746c777d256250c489131474
Gabriel Prochnau <2288@holbertonschool.com>
2021-05-09 20:30:57 +0000

    Fix typo

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,6 +7,6 @@ app_views = Blueprint("/api/v1", __name__)
 
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.sities import *
+from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
